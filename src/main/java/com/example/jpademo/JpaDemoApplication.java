7efb626c8dd3d86e1e67ddf612dc633c33d23a78
romilksolves <67729828+romilksolves@users.noreply.github.com>
2022-03-01 19:48:07 +0530

    Update JpaDemoApplication.java

--- src/main/java/com/example/jpademo/JpaDemoApplication.java
+++ src/main/java/com/example/jpademo/JpaDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaDemoApplication {
 
+	// Entry point method
 	public static void main(String[] args) {
 		SpringApplication.run(JpaDemoApplication.class, args);
 	}
